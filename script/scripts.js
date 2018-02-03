c3efdef0a2924333fe9fdee9a6bcb14fe7d10b85
your_name <your_email>
2018-02-03 12:37:53 -0800

    add workshop project to git

---
+++ script/scripts.js
@@ -0,0 +1,12 @@
+alert("Hello Universe!");
+alert(2 + 2);
+alert(4 * "2");
+alert(8 +"2");
+$(document).ready(function() {
+  alert("jQuery is working");
+  alert($("#css-item").text());
+  $("#goal-button").click(function() {
+    $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+    $("#new-goal").val("");
+})
+})
